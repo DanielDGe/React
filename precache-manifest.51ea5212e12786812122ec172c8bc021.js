2ba809268fbd9096e306130182fc5a47ceb42f8d
Daniel <ddge07@gmail.com>
2020-09-18 14:57:07 -0500

    Updates

---
+++ precache-manifest.51ea5212e12786812122ec172c8bc021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f45794eeb0cd4b3b67494b28e0e15c",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "4b177feae73ed4982ce5",
+    "url": "/React/static/css/main.095bc103.chunk.css"
+  },
+  {
+    "revision": "7bec9e32f92fbad21d70",
+    "url": "/React/static/js/2.a91934ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React/static/js/2.a91934ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b177feae73ed4982ce5",
+    "url": "/React/static/js/main.d0b5a367.chunk.js"
+  },
+  {
+    "revision": "5283b7642056c2f0ec26",
+    "url": "/React/static/js/runtime-main.04561ca8.js"
+  }
+]);
